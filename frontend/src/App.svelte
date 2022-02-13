176bea9b78208a027cfacf8f6b32852bccff5776
bong-u <bongudev@gmail.com>
2022-02-13 14:58:49 +0000

    fetch items

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,51 @@
 <script>
-	export let name;
+	
+	$ : items = fetch('http://bongg.run.goorm.io/item')
+	.then( resp => {
+		return resp.json();
+	})
+	.then( data => {
+		console.log(JSON.stringify(data));
+		return data;
+		
+	})
+	.catch(error => {
+		console.error(error)
+		return [];
+	});
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>REST</h1>
+	
+	{#await items}
+    	<p>...Loading</p>
+		
+	{:then items } 
+    	<ul>
+      	{#each items as item, index}
+        	<li>
+          		<p>[{item.id}] {item.name}</p>
+       		</li>
+		{/each}
+    	</ul>    
+		
+  	{:catch error}
+    	<p>오류가 발생했습니다.</p>
+  	{/await}
+	
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h1 {
+        color: #ff3e00;
+        font-size: 4em;
+        font-weight: 100;
+    }
 </style>
