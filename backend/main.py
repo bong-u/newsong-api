6e5c252d050e3dd81045e017b89311c94537c6b8
bong-u <bongudev@gmail.com>
2022-02-13 13:56:13 +0000

    fastAPI with svelte

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+
+import uvicorn
+
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from routers import rest
+
+templates = Jinja2Templates(directory='../svelte/public/')
+
+app = FastAPI()
+
+app.mount('/build', StaticFiles(directory='../svelte/public/build'), name='static')
+app.include_router(rest.router)
+
+@app.get('/')
+def render(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=80)
