662688ce2000d1c14bf02c6ec6c4838f30469b45
bong-u <bongudev@gmail.com>
2022-03-26 14:57:31 +0000

    login implemented

--- frontend/src/login.svelte
+++ frontend/src/login.svelte
@@ -1,21 +1,70 @@
 <script>
-	let user_id = '';
-	let pw = '';
+	let logined = false;
+	let token = '';
+	let username = '';
 	
 	function loginProc() {
-		console.log(user_id);
+		let formData = new FormData(document.getElementById('loginForm'));
 		
+		fetch('http://bongg.run.goorm.io/login', {
+			method: 'POST',
+			body : formData,
+		})
+		.then( resp => {
+			return resp.json();
+		})
+		.then( data => {
+			if (! data.access_token) {		//login failed
+				alert (data.detail);
+				return;
+			}
+			
+			logined = true;
+			token = data.access_token;
+			username = formData.get('username');
+			return;
+		})
+		.catch(error => {
+			console.log (error)
+			console.error(error)
+			return;
+		});
 	}
+	
+	function search() {
+		console.log(document.getElementById('query').value)
+	
+		fetch('https://www.music-flo.com/api/search/v2/search?keyword='+query+'&searchType=ARTIST&sortType=ACCURACY&size=10&page=1')
+		.then( resp => {
+			return resp.json();
+		})
+		.then( data => {
+		})
+		.catch(error => {
+		});
+	}
+		
 </script>
 
 <main>
-	<form on:submit|preventDefault={ loginProc }>
+	<form id="loginForm" on:submit|preventDefault={ loginProc }>
 	
-		<input type="text" name="id" bind:value={user_id}/>
-		<input type="password" name="password" bind:value={pw}/>
+		<input type="text" name="username"/>
+		<input type="password" name="password"/>
 		
 		<button>login</button>
 	</form>
+	
+	{#if logined||true}
+		{ username } logined.
+		<form id="searchForm" on:submit|preventDefault={ search }>
+			<input id="query" type="text"/>
+			<button>search</button>
+		</form>
+		
+		<div id="searchArea">
+		</div>
+	{/if}
 </main>
 
 <style>
