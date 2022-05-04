edb6de791229533273518bc875829e95f9140580
bong-u <bongudev@gmail.com>
2022-05-04 14:29:07 +0000

    useless comment removed

--- backend/rest/database.py
+++ backend/rest/database.py
@@ -10,12 +10,3 @@ engine = create_engine (config('DATABASE_URL'))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# import peewee
-
-
-# DATABASE_NAME = config('DATABASE')
-# db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
-# db_state = ContextVar("db_state", default=db_state_default.copy())
-
-# db = peewee.SqliteDatabase(DATABASE_NAME, check_same_thread=False)
